896f79c9655963daeffa5e69c92c68daecae2640
Ólafur Helgi Jónsson <olli.jonsson@gmail.com>
2019-08-20 14:15:28 +0000

    better

--- server.py
+++ server.py
@@ -10,52 +10,47 @@ from twisted.python import log
 from twisted.web.server import Site
 from twisted.web.static import Data
 
-from utils.model import DeepSpeech
 from utils.thread_with_trace import thread_with_trace
+from streaming.stream import Stream
 
-from streaming.stream import stream
-from streaming.vadaudio import VADAudio
 
-from config import config
-
-ds_small = DeepSpeech(config, 'small_lm')
-#ds_large = DeepSpeech(config, 'large_lm')
-
-class Stream(WebSocketServerProtocol):
+class Anna(WebSocketServerProtocol):
 
 
     def onConnect(self, request):
         print("Client connecting: {0}".format(request.peer))
-        self.vad_audio = VADAudio(aggressiveness=3,
-                            device=None,
-                            input_rate=16000)
+        self.stream = Stream()
         self.run_stream = thread_with_trace(
-            target=stream,
-            args=(ds_small, self.vad_audio, self, ))
+            target=self.stream.run,
+            args=(self, ))
         self.run_stream.start()
+
 
     def onClose(self, wasClean, code, reason):
         print("WebSocket connection closed: {0}".format(reason))
-        self.vad_audio = None
+        self.stream.destroy_self()
+        self.stream = None
         self.run_stream.kill()
         self.run_stream.join()
 
-    # def onMessage(self, payload, isBinary):
-    #    lm = json.loads(payload)
+
+    def onMessage(self, payload, isBinary):
+        print("toggled")
+        self.stream.toggle_lm()
 
 
 if __name__ == '__main__':
 
     log.startLogging(sys.stdout)
 
-    streamFactory = WebSocketServerFactory()
-    streamFactory.protocol = Stream
-    streamFactory.startFactory()
-    sResource = WebSocketResource(streamFactory)
+    annaFactory = WebSocketServerFactory()
+    annaFactory.protocol = Anna
+    annaFactory.startFactory()
+    aResource = WebSocketResource(annaFactory)
 
     # Establish a dummy root resource
     root = Data("", "text/plain")
-    root.putChild(b"stream", sResource)
+    root.putChild(b"stream", aResource)
 
     # both under one Twisted Web Site
     site = Site(root)
--- streaming/stream.py
+++ streaming/stream.py
@@ -1,47 +1,101 @@
 from config import config
+from utils.model import DeepSpeech
 from utils.parse import process_file
+from streaming.vadaudio import VADAudio
 import scipy.io.wavfile as wav
 import numpy as np
 import os
 import json
 
+class Stream(VADAudio):
+    
+    def __init__(
+        self, 
+        aggressiveness=3, 
+        device=None, 
+        input_rate=16000):
 
-def handle_output(prev_output, output, ws):
-    if (prev_output != output):
-        print('intermediate: ', output)
-        prev_output = output
-        obj = {
-            'type': 'intermediate',
-            'transcript': output,
-        }
-        payload = json.dumps(obj, ensure_ascii=False).encode('utf8')
-        ws.sendMessage(payload)
-    return prev_output
+        super().__init__(
+            aggressiveness=aggressiveness, 
+            device=device, 
+            input_rate=input_rate)
 
+        self.ds_aime = DeepSpeech(config, 'small_lm')
+        self.ds_large = None
 
-def stt(ds, wav_data):
-    filename = process_file(wav_data)
-    fs, audio = wav.read(filename)
-    stt = ds.stt(audio, fs)
-    os.remove(filename)
-    return stt
-
-
-def stream(ds, vad_audio, ws):
-    frames = vad_audio.vad_collector()
-    wav_data = bytearray()
-
-    for frame in frames:
-        if frame is not None:
-            wav_data.extend(frame)
+    def toggle_lm(self):
+        if not self.ds_large:
+            self.ds_large = DeepSpeech(config, 'large_lm')
         else:
-            transcript_stt = stt(ds, wav_data)
-            print('aime-lm: ', transcript_stt)
+            self.ds_large.__del__()
+            self.ds_large = None
+            self.sctxt = None
+
+    def stt(self, wav_data):
+        filename = process_file(wav_data)
+        fs, audio = wav.read(filename)
+        transcript = self.ds_aime.stt(audio, fs)
+        os.remove(filename)
+        return transcript
+    
+    def handle_output(self, prev_output, output, ws):
+        if (prev_output != output):
+            print('intermediate: ', output)
+            prev_output = output
             obj = {
-                'type': 'aime',
-                'transcript': transcript_stt,
+                'type': 'intermediate',
+                'transcript': output,
             }
             payload = json.dumps(obj, ensure_ascii=False).encode('utf8')
             ws.sendMessage(payload)
+        return prev_output
 
-            wav_data = bytearray()
+    def destroy_self(self):
+        self.ds_aime.__del__()
+        if self.ds_large:
+            self.ds_large.__del__()
+    
+    def run(self, ws):
+        frames = self.vad_collector()
+        wav_data = bytearray()
+        prev_output = ''
+        output = ''
+        counter = 0
+        self.sctxt = None
+        if self.ds_large:
+            self.sctxt = self.ds_large.setupStream()
+        
+        for frame in frames:
+            if frame is not None:
+                wav_data.extend(frame)
+                if self.ds_large and self.sctxt:
+                    self.ds_large.feedAudioContent(
+                        self.sctxt, np.frombuffer(frame, np.int16))
+                    if counter == 8:
+                        output = self.ds_large.intermediateDecode(self.sctxt)
+                        prev_output = self.handle_output(prev_output, output, ws)
+                        counter = 0
+                    else:
+                        counter += 1
+            else:
+                if self.ds_large and self.sctxt:
+                    transcript = self.ds_large.finishStream(self.sctxt)
+                    print('transcript: ', transcript)
+                    obj = {
+                        'type': 'final',
+                        'transcript': transcript,
+                    }
+                    payload = json.dumps(obj, ensure_ascii=False).encode('utf8')
+                    ws.sendMessage(payload)
+                transcript_stt = self.stt(wav_data)
+                print('aime-lm: ', transcript_stt)
+                obj = {
+                    'type': 'aime',
+                    'transcript': transcript_stt,
+                }
+                payload = json.dumps(obj, ensure_ascii=False).encode('utf8')
+                ws.sendMessage(payload)
+
+                wav_data = bytearray()
+                if self.ds_large:
+                    self.sctxt = self.ds_large.setupStream()
